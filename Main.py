3c2d66165ffae607a23e1548d9a2abb10e3dab16
gabriel <21123@studnet.smrgs.school.nz>
2024-05-24 12:32:28 +1200

    made everything neater by seperating the different components

--- Main.py
+++ Main.py
@@ -1,20 +1,3 @@
-from tkinter import*
-w_width = 500
-w_height = 800
-bg_colour = "#E7DDFF"
-window = Tk()
-window.geometry("500x500")
-window.geometry(str(w_width) + "x" + str(w_height))
-window.title("windoe")
-
-top_frame = Frame(background ='yellow' , width=w_width, height=100)
-top_frame.pack()
-
-main_frame = Frame(background=bg_colour , width=w_width, height=(w_height-200))
-main_frame.pack()
-
-bottom_frame = Frame(background = 'cyan', width=w_width, height=150)
-bottom_frame.pack(side='bottom')
-
-start_button = Button(bottom_frame, text="start", height=5, width=5, bg='pink')
-window.mainloop()
+from application import *
+if __name__ == "__main__":
+    app = App()
