5cba11bc2aad7ce6055a323d88a84466963ae622
gabriel <21123@students.mrgs.school.nz>
2024-05-30 13:23:46 +1200

    ADDED BUTTONS

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from appsettings import *
-
+from app_settings import *
+from os import *
 w_width = 500
 w_height = 800
 bg_colour = "#E7DDFF"
@@ -11,23 +11,23 @@ class App:
         self.window.geometry(str(w_width) + "x" + str(w_height))
         self.window.title("window")
 
-        self.top_frame = Frame(self.window, background='yellow', width=500, height=100)
+        self.top_frame = Frame(self.window, background='white', width=500, height=100)
         self.top_frame.pack(fill='x')
 
         self.main_frame = Frame(self.window, background=bg_colour, width=500, height=(w_height-200))
         self.main_frame.pack(fill='both', expand=True)
 
-        self.bottom_frame = Frame(self.window, background='cyan', width=500, height=150)
-        self.bottom_frame.pack(fill='x', side='bottom')
+        self.bottom_frame = Frame(self.window, background=bg_colour, width=500, height=150)
+        self.bottom_frame.pack()
 
-        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='green')
-        self.home_button.place(x=0, y=0)
+        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='white')
+        self.home_button.pack(side=LEFT, padx=10, pady=5)
 
-        self.back_button = Button(self.top_frame, text="btih", height=2, width=5, bg='purple')
-        self.back_button.place(x=0, y=0)
+        self.back_button = Button(self.bottom_frame, text="Quiz", height=2, width=5, bg='white')
+        self.back_button.pack(side=LEFT, padx=100, pady=30)
 
-        self.exit_button = Button(self.main_frame, text="outta this house", height=2, width=5, bg='red', command=self.exit)
-        self.exit_button.place(x=0, y=5)
+        self.exit_button = Button(self.bottom_frame, text="exit", height=2, width=5, bg='white', command=self.exit)
+        self.exit_button.pack(side=LEFT, padx=10, pady=5)
 
         self.window.mainloop()
 
--- Main.py
+++ Main.py
@@ -1,3 +1,3 @@
-from application import *
+from App import *
 if __name__ == "__main__":
     app = App()
