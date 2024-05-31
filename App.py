4c88fee2d06ac55ef6b99c616f96a35925fb56a3
Gabriel <21123@students.mrgs.school.nz>
2024-05-31 12:31:54 +1200

    Added an Image to the home page

--- App.py
+++ App.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from appsettings import *
-
+from PIL import ImageTk, Image
+from app_settings import *
 w_width = 500
 w_height = 800
 bg_colour = "#E7DDFF"
@@ -11,23 +11,37 @@ class App:
         self.window.geometry(str(w_width) + "x" + str(w_height))
         self.window.title("window")
 
-        self.top_frame = Frame(self.window, background='yellow', width=500, height=100)
+        self.home_frame = Frame(self.window, background='gray', width=w_width, height=(w_height-200))
+        self.home_frame.pack()
+
+        # Load the image
+        image = Image.open("imgs/Book.png")
+        photo = ImageTk.PhotoImage(image)
+
+        # Create the label with the image in self.home_frame
+        bg_image = Label(self.home_frame, image=photo)
+        bg_image.image = photo  # Keep a reference to the image to prevent garbage collection
+        bg_image.pack()
+
+        self.top_frame = Frame(self.window, background='white', width=500, height=100)
         self.top_frame.pack(fill='x')
 
+        ## Main ##
         self.main_frame = Frame(self.window, background=bg_colour, width=500, height=(w_height-200))
         self.main_frame.pack(fill='both', expand=True)
 
-        self.bottom_frame = Frame(self.window, background='cyan', width=500, height=150)
-        self.bottom_frame.pack(fill='x', side='bottom')
+        ## END ##
+        self.bottom_frame = Frame(self.window, background=bg_colour, width=500, height=150)
+        self.bottom_frame.pack()
 
-        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='green')
-        self.home_button.place(x=0, y=0)
+        self.home_button = Button(self.bottom_frame, text="Home", height=2, width=5, bg='white')
+        self.home_button.pack(side=LEFT, padx=10, pady=5)
 
-        self.back_button = Button(self.top_frame, text="btih", height=2, width=5, bg='purple')
-        self.back_button.place(x=0, y=0)
+        self.back_button = Button(self.bottom_frame, text="Quiz", height=2, width=5, bg='white')
+        self.back_button.pack(side=LEFT, padx=100, pady=30)
 
-        self.exit_button = Button(self.main_frame, text="outta this house", height=2, width=5, bg='red', command=self.exit)
-        self.exit_button.place(x=0, y=5)
+        self.exit_button = Button(self.bottom_frame, text="exit", height=2, width=5, bg='white', command=self.exit)
+        self.exit_button.pack(side=LEFT, padx=10, pady=5)
 
         self.window.mainloop()
 
